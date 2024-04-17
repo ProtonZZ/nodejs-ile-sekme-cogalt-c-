cdf72e4f930c952533d79b70f3f4dffd24ef907e
ProtonZZ <110591322+ProtonZZ@users.noreply.github.com>
2024-04-17 22:36:19 +0300

    Add files via upload
    açılan sekmelerin logu logs.html yansır  // pek uğraşmadım  linkleri değiştirerek sürekli sekme açtırabilirsiniz // ( bu script tamamen eğlence için yapılmıştır herhangi bir olumsuz davranıştan sorumlu değilim) iyi şanslar

---
+++ index.js
@@ -0,0 +1,52 @@
+﻿  const open = require('opn');
+
+let logId = 1;
+
+setInterval(() => {
+  open('https://github.com/ProtonZZ/');
+  console.log('Requesting resource from target link');
+}, 5000);
+
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        console.error(`Error: ${err}`);
+        res.end();
+        return;
+}
+      res.end(data);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('mustoxs');
+  setInterval(() => {
+    const log = {
+      id: Date.now(),
+      timestamp: new Date().toLocaleString(),
+      status: 'basarılı',
+      url: 'https://github.com/ProtonZZ/'
+    };
+    console.log(log);
+    fs.appendFile('logs.html', `
+      
+                                <tr><td></td><td>&nbsp;&nbsp;${log.id}</td></tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+                            `<tr><td></td><td>&nbsp;&nbsp;${log.timestamp}</td></tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+                             `<tr><td></td><td>&nbsp;&nbsp;${log.status}</td></tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+                             `<tr><td></td><td>&nbsp;&nbsp;${log.url}</td></tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+                             '<tr><td colspan="2"><hr></td></tr>\n', (err) => {
+  if (err) {
+    console.error(err);
+        return;
+      }
+   console.log(`Log added to logs.html : ${JSON.stringify(log)}`);
+    });
+  }, 5000);
+});
